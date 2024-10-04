46926b79e9e80081ffb535534ad131ce11c05c0d
Rahyuni <hanrahyun@naver.com>
2024-10-04 12:05:24 +0900

    ✨ [add] Decorator - 스터디용 코드 추가 : coffee

---
+++ decorator/DecoratorStudyCode/src/Coffee/SugarDecorator.java
@@ -0,0 +1,17 @@
+package Coffee;
+
+public class SugarDecorator extends CoffeeDecorator {
+    public SugarDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + ", Sugar";
+    }
+
+    @Override
+    public double getCost() {
+        return super.getCost() + 0.5;
+    }
+}
